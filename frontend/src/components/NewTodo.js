01b707b98e34705a25014e8a93ca2be74601a8fb
Jan Robert <janmartinrobert@gmail.com>
2021-08-09 12:12:00 +0200

    Add NewTodo component

---
+++ frontend/src/components/NewTodo.js
@@ -0,0 +1,10 @@
+import './NewTodo.css'
+
+export default function NewTodo(props) {
+  return (
+    <section className="new-todo">
+      <input type="text" />
+      <button>Add</button>
+    </section>
+  )
+}
