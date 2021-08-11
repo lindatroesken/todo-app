e471de33a43fa8e11f3adb9173d8e31a23f82031
Jan Robert <janmartinrobert@gmail.com>
2021-08-11 17:32:40 +0200

    Switch to styled components

--- frontend/src/components/NewTodo.js
+++ frontend/src/components/NewTodo.js
@@ -1,6 +1,6 @@
-import './NewTodo.css'
 import { useState } from 'react'
 import PropTypes from 'prop-types'
+import styled from 'styled-components/macro'
 
 NewTodo.propTypes = {
   onAdd: PropTypes.func.isRequired,
@@ -14,13 +14,19 @@ export default function NewTodo({ onAdd }) {
   }
 
   return (
-    <section className="new-todo">
+    <Wrapper>
       <input
         type="text"
         value={description}
         onChange={event => setDescription(event.target.value)}
       />
       <button onClick={handleAddClick}>Add</button>
-    </section>
+    </Wrapper>
   )
 }
+
+const Wrapper = styled.section`
+  display: grid;
+  grid-template-columns: 1fr min-content;
+  padding: 12px;
+`
