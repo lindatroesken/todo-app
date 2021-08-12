1642f8901d035c0fd154cac531e24f4b53867aae
Jan Robert <janmartinrobert@gmail.com>
2021-08-12 17:04:54 +0200

    Extract useEditForm hook

--- frontend/src/components/TodoForm.js
+++ frontend/src/components/TodoForm.js
@@ -1,31 +1,11 @@
-import { useState } from 'react'
-import { useHistory } from 'react-router-dom'
 import styled from 'styled-components/macro'
+import useEditForm from '../hooks/useEditForm'
 
 export default function TodoForm({ todo, onSave }) {
-  const [formData, setFormData] = useState({
-    description: todo.description,
-    status: todo.status,
-  })
-  const history = useHistory()
-
-  const handleSubmit = event => {
-    event.preventDefault()
-    const updatedTodo = { ...todo, ...formData }
-    onSave(updatedTodo).then(() => history.push('/'))
-  }
-
-  const resetForm = () => {
-    setFormData({
-      description: todo.description,
-      status: todo.status,
-    })
-  }
-
-  const handleChange = event => {
-    const { name, value } = event.target
-    setFormData({ ...formData, [name]: value })
-  }
+  const { formData, handleSubmit, handleChange, resetForm } = useEditForm(
+    todo,
+    onSave
+  )
 
   return (
     <Wrapper onSubmit={handleSubmit}>
